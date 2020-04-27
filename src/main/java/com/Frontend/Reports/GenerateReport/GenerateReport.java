ff0a2006e8af3101c1d003af786c1f4cf835e816
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-04-27 18:06:41 -0400

    Added Group Report Functionality to General Group

--- src/main/java/com/Frontend/Reports/GenerateReport/GenerateReport.java
+++ src/main/java/com/Frontend/Reports/GenerateReport/GenerateReport.java
@@ -15,19 +15,20 @@ import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.formlayout.FormLayout.ResponsiveStep;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.grid.GridVariant;
-import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.html.H5;
-import com.vaadin.flow.component.html.H6;
+import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
 import java.time.LocalDate;
 import java.util.List;
 
+import static com.Backend.MySQLMethods.round;
+
 @Route(value = "report", layout = MainView.class)
+@PageTitle("FBLA Genie | Generate Report")
 public class GenerateReport extends VerticalLayout {
     final Button individualReport = new Button("Generate Individual Report", buttonClickEvent -> generateIndividualReport());
     final Button groupReport = new Button("Generate Group Report", buttonClickEvent -> generateGroupReport());
@@ -51,16 +52,20 @@ public class GenerateReport extends VerticalLayout {
         add(form);
     }
 
-    public void generateGroupReport() {
-        removeAll();
-        FormLayout form = new FormLayout();
-        form.setResponsiveSteps(
-                new ResponsiveStep("20em", 1),
-                new ResponsiveStep("30em", 2),
-                new ResponsiveStep("40em", 3));
+    /**
+     * This method returns a the pre-formatted data of a student. This simplifies the job significantly when
+     * writing multiple pieces of data
+     *
+     * @param header The heading text of the piece of data
+     * @param text   The piece of data
+     * @return A Div containing the formatted Student Data
+     */
+    public static Div setText(String header, String text) {
+        Div div = new Div();
+        div.add(new H6(header + ":"));
+        div.add(text);
 
-        individualReport.setText("Generate Individual Report Instead");
-
+        return div;
     }
 
     public void generateIndividualReport() {
@@ -155,6 +160,70 @@ public class GenerateReport extends VerticalLayout {
         add(form, actions);
     }
 
+    public static void nextSection(Board dataBoard) {
+        //Adding two lines to signify the end of a section
+        dataBoard.addRow(new Html("<hr>"));
+        dataBoard.addRow(new Html("<hr>"));
+    }
+
+    public static Div makeBulletList(String label, List<StudentData> list) {
+        Html header = new Html("<h4 style = \"color:#800517\">" + label + "</h4>");
+        StringBuilder html = new StringBuilder("<ul style = \"color:#c11b17\">");
+        for (StudentData s : list) {
+            html.append("<li>").append(s).append("</li>");
+        }
+        html.append("</ul>");
+        Html bullet = new Html(String.valueOf(html));
+        Div div = new Div(header);
+        div.add(bullet);
+        return div;
+    }
+
+    /**
+     * Calculates the hours remaining for a student to reach his or her community service goal
+     *
+     * @param student The Student who is being analyzed
+     * @return A String containing formatted hours to go
+     */
+    private String getHoursRemaining(StudentData student) {
+        String category = student.getCommunityServiceCategory();
+        double hours = student.getCommunityServiceHours();
+        if (category.contains("50")) {
+            if (hours >= 50) return "Goal Reached! Next Goal: CSA Service (200 Hours)";
+            else return (50 - hours) + " Hours To Go";
+        } else if (category.contains("200")) {
+            if (hours >= 200) return "Goal Reached! Next Goal: CSA Achievement (500 Hours)";
+            else return (200 - hours) + " Hours To Go";
+        } else {
+            assert category.contains("500");
+            if (hours >= 500) return "Completed the Highest Goal! Congratulations!";
+            else return (500 - hours) + "To Go";
+        }
+    }
+
+    public void generateGroupReport() {
+        removeAll();
+        FormLayout form = new FormLayout();
+        form.setResponsiveSteps(
+                new ResponsiveStep("20em", 1),
+                new ResponsiveStep("30em", 2),
+                new ResponsiveStep("40em", 3));
+
+        individualReport.setText("Generate Individual Report Instead");
+
+        Button selector = new Button("Generate Group Report");
+        selector.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        ComboBox<String> rangeSelector = new ComboBox<>("Range of Data", Charts.WEEK_CHART, Charts.MONTH_CHART, Charts.YEAR_CHART, Charts.ALL_TIME_CHART);
+        rangeSelector.setValue(Charts.ALL_TIME_CHART);
+        add(individualReport, rangeSelector, selector);
+        selector.addClickListener(onClick -> {
+            remove(selector);
+            groupReport(rangeSelector.getValue());
+        });
+        setAlignItems(Alignment.CENTER);
+        setAlignSelf(Alignment.CENTER);
+    }
+
     public void individualReport(Student student, Date startingDate) {
         removeAll();
         StudentData dataOfStudent = student.getStudentData();
@@ -167,7 +236,7 @@ public class GenerateReport extends VerticalLayout {
             eventsList = MySQLMethods.selectStudentEventsInRange(student, startingDate);
         }
         double hoursInRange = Event.getTotalHours(eventsList);
-        double totalHours = MySQLMethods.round(dataOfStudent.getCommunityServiceHours());
+        double totalHours = round(dataOfStudent.getCommunityServiceHours());
 
         //Adds space between edge of screen and content
         setPadding(true);
@@ -276,51 +345,117 @@ public class GenerateReport extends VerticalLayout {
         add(dataBoard);
     }
 
-    public void groupReport() {
-
-    }
-
-    /**
-     * This method returns a the pre-formatted data of a student. This simplifies the job significantly when
-     * writing multiple pieces of data
-     *
-     * @param header The heading text of the piece of data
-     * @param text   The piece of data
-     * @return A Div containing the formatted Student Data
-     */
-    public static Div setText(String header, String text) {
-        Div div = new Div();
-        div.add(new H6(header + ":"));
-        div.add(text);
-
-        return div;
-    }
-
-    public static void nextSection(Board dataBoard) {
-        //Adding two lines to signify the end of a section
-        dataBoard.addRow(new Html("<hr>"));
-        dataBoard.addRow(new Html("<hr>"));
-    }
-
-    /**
-     * Calculates the hours remaining for a student to reach his or her community service goal
-     *
-     * @param student The Student who is being analyzed
-     * @return A String containing formatted hours to go
-     */
-    private String getHoursRemaining(StudentData student) {
-        String category = student.getCommunityServiceCategory();
-        double hours = student.getCommunityServiceHours();
-        if (category.contains("50")) {
-            if (hours >= 50) return "Goal Reached! Next Goal: CSA Service (200 Hours)";
-            else return (50 - hours) + " Hours To Go";
-        } else if (category.contains("200")) {
-            if (hours >= 200) return "Goal Reached! Next Goal: CSA Achievement (500 Hours)";
-            else return (200 - hours) + " Hours To Go";
-        } else {
-            assert category.contains("500");
-            if (hours >= 500) return "Completed the Highest Goal! Congratulations!";
-            else return (500 - hours) + "To Go";
+    public void groupReport(String option) {
+        removeAll();
+        Board dataBoard = new Board();
+        //This list contains all of the students in the chapter for easy reference for the rest of the report
+        List<StudentData> studentDataList = MySQLMethods.getStudentData();
+        List<StudentData> studentDataListInRange = MySQLMethods.getStudentData(option);
+        List<StudentData> inactiveStudentList = StudentData.removeActive(studentDataListInRange);
+        int activeMembers = 0;
+        double totalHours = 0;
+        for (StudentData s : studentDataListInRange) {
+            if (s.getCommunityServiceHours() > 0) {
+                activeMembers++;
+                totalHours += s.getCommunityServiceHours();
+            }
         }
+
+        /* Section 1: General Information
+         * This section is mainly for a brief overview. It will give a few numbers
+         * and overarching data which is important for most when looking for basic information
+         *
+         * This section will include, but is not limited to:
+         *   Number of Students
+         *   Average Student Hours
+         *   Average Student Hours Discounting all Zeros
+         *   Number of Students without hours
+         *   Average Community Service Category goal
+         *   Average Achieved Community Service Category
+         */
+        H1 generalInformation = new H1("General Information");
+        dataBoard.addRow(generalInformation);
+
+        H2 chapterDetails = new H2("Chapter Details");
+        Div numMembers = setText("Number of Chapter Members", Integer.toString(studentDataListInRange.size()));
+        Div numActiveMembers = setText("Number of Active Members", Integer.toString(activeMembers)); //Note: An Active Member is someone who has more than 0 hours
+        Div percentActiveMembers = setText("Percent of Members Active", new Percent(activeMembers, studentDataListInRange.size()).toString());
+        Div numHours = setText("Total Hours In Chapter", Double.toString(totalHours));
+
+        //Heading for Section
+        dataBoard.addRow(chapterDetails);
+
+        //Add A Few Details
+        dataBoard.addRow(numMembers, numActiveMembers, percentActiveMembers, numHours);
+
+        nextSection(dataBoard);
+
+        /*
+         * A Section for the Specific Member Details, such as Average Hours and such
+         */
+        H2 memberDetails = new H2("Member Details");
+        Div averageHours = setText("Average Hours Per Student", Double.toString(round(totalHours / studentDataListInRange.size())));
+        Div averageCategory = setText("Average Achieved Community Service Category", StudentData.getAverageCategory(studentDataListInRange));
+        Div averageGoal = setText("Average Community Service Goal", StudentData.getAverageGoal(studentDataListInRange));
+
+        Div averageActiveHours = setText("Average Hours Per Active Student", "0 (0 Active Students)");
+        Div averageActiveCategory = setText("Average Achieved Community Service Category by Active Students", "None (No Active Students)");
+        Div averageActiveGoal = setText("Average Community Service Goal of Active Students", "None (No Active Students)");
+
+        if (activeMembers > 0) {
+            averageActiveHours = setText("Average Hours Per Active Student", Double.toString(round(totalHours / activeMembers)));
+            averageActiveCategory = setText("Average Achieved Community Service Category by Active Students", StudentData.getActiveCategory(studentDataListInRange));
+            averageActiveGoal = setText("Average Community Service Goal of Active Students", StudentData.getActiveGoal(studentDataListInRange));
+        }
+
+        //Heading for Section
+        dataBoard.addRow(memberDetails);
+
+        //A Few general, all-student details
+        dataBoard.addRow(averageHours, averageCategory, averageGoal);
+
+        //More Specific Details with only activeStudents
+        dataBoard.addRow(averageActiveHours, averageActiveCategory, averageActiveGoal);
+
+        nextSection(dataBoard);
+
+        //This next section is to help visualize the data
+        Chart chart = Charts.contributionTreemapChart(option);
+        dataBoard.addRow(chart);
+        if (inactiveStudentList.size() > 0) {
+            Div inactiveStudentsHTML = makeBulletList("Inactive Students", inactiveStudentList);
+            dataBoard.addRow(inactiveStudentsHTML);
+        }
+
+        /*
+         *  Section 2: Community Service Award Analysis
+         *
+         * This section is for an overview of the Community Service Award Categories and their achievements. This will
+         * help in analyzing the goals and achievements of each student, differentiating between the active and inactive students
+         *
+         * This Section will include, but is not limited to:
+         *    A Bar Graph for goals and achievements
+         *    A Pie Chart for how many people achieved their goals
+         */
+        H1 csaAnalysis = new H1("Community Service Award Analysis");
+        dataBoard.addRow(csaAnalysis);
+        H2 evolutionChart = new H2("Community Service Division Bar/Pie Graph");
+
+        Chart goalChart = Charts.barGraphCommunityServiceCategoryGoals(studentDataList);
+        Chart achievedChart = Charts.barGraphCommunityServiceCategoryAchieved(studentDataList);
+
+        dataBoard.addRow(evolutionChart);
+        dataBoard.addRow(goalChart, achievedChart);
+        dataBoard.addRow(new Html("<hr>"));
+        dataBoard.addRow(Charts.goalDivisionChart(studentDataList, "Student Goal Division"),
+                Charts.currentDivisionChart(studentDataList, "Student Current Division"));
+
+        nextSection(dataBoard);
+
+        dataBoard.addRow(new H2("Community Service Goal Achievement Analysis"));
+        dataBoard.addRow(Charts.achievedGoalPieChart(studentDataList, "Students Who Achieved vs Didn't Achieve Their Goals"));
+
+
+        add(dataBoard);
     }
 }
