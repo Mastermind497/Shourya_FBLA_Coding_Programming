6da99d45c7b091d30998b80db9e9ef85b87c45b0
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-04-29 15:10:59 -0400

    Added Comments to Security Methods

--- src/main/java/com/frontend/Security/SecurityUtils.java
+++ src/main/java/com/frontend/Security/SecurityUtils.java
@@ -9,20 +9,28 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import javax.servlet.http.HttpServletRequest;
 import java.util.stream.Stream;
 
+/**
+ * A Class for Util methods in security
+ */
 public final class SecurityUtils {
 
     private SecurityUtils() {
         // Util methods only
     }
 
-    static boolean isFrameworkInternalRequest(HttpServletRequest request) { //
+    static boolean isFrameworkInternalRequest(HttpServletRequest request) {
         final String parameterValue = request.getParameter(ApplicationConstants.REQUEST_TYPE_PARAMETER);
         return parameterValue != null
                 && Stream.of(ServletHelper.RequestType.values())
                 .anyMatch(r -> r.getIdentifier().equals(parameterValue));
     }
 
-    static boolean isUserLoggedIn() { //
+    /**
+     * Checks if any user is logged in
+     *
+     * @return whether or not anyone is logged in
+     */
+    static boolean isUserLoggedIn() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return authentication != null
                 && !(authentication instanceof AnonymousAuthenticationToken)
