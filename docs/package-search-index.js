172ba737aa8bedafc287410824a7547017f316cd
Shourya Bansal <39387439+Mastermind497@users.noreply.github.com>
2020-05-03 16:26:40 -0400

    Added JavaDoc

---
+++ docs/package-search-index.js
@@ -0,0 +1,4 @@
+packageSearchIndex = [{
+    "l": "All Packages",
+    "url": "allpackages-index.html"
+}, {"l": "com.backend"}, {"l": "com.frontend"}, {"l": "com.frontend.Add.AddHours"}, {"l": "com.frontend.Add.CreateStudent"}, {"l": "com.frontend.Documentation"}, {"l": "com.frontend.GetStudentInformation"}, {"l": "com.frontend.Login"}, {"l": "com.frontend.Reports"}, {"l": "com.frontend.Security"}]
---
+++ docs/type-search-index.js
@@ -0,0 +1,25 @@
+typeSearchIndex = [{"p": "com.frontend.Add.AddHours", "l": "AddHours"}, {
+    "l": "All Classes",
+    "url": "allclasses-index.html"
+}, {"p": "com.frontend", "l": "Application"}, {"p": "com.frontend", "l": "Charts"}, {
+    "p": "com.frontend.Security",
+    "l": "ConfigureUIServiceInitListener"
+}, {"p": "com.frontend.Add.CreateStudent", "l": "CreateStudent"}, {
+    "p": "com.backend",
+    "l": "Date"
+}, {"p": "com.frontend.Documentation", "l": "Documentation"}, {
+    "p": "com.backend",
+    "l": "Event"
+}, {"p": "com.frontend.GetStudentInformation", "l": "GetStudentInformation"}, {
+    "p": "com.frontend",
+    "l": "Home"
+}, {"p": "com.frontend.Login", "l": "LoginView"}, {"p": "com.frontend", "l": "MainView"}, {
+    "p": "com.backend",
+    "l": "MySQLMethods"
+}, {"p": "com.backend", "l": "Percent"}, {"p": "com.frontend.Reports", "l": "Reports"}, {
+    "p": "com.frontend.Security",
+    "l": "SecurityConfiguration"
+}, {"p": "com.frontend.Security", "l": "SecurityUtils"}, {"p": "com.backend", "l": "Student"}, {
+    "p": "com.backend",
+    "l": "StudentData"
+}]
